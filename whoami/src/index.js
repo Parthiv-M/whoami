7a64637d29de5b3c1d56f9bbded2b6fb44a9d430
theProton <parthivmenon.dev@gmail.com>
2021-01-14 15:36:39 +0530

    initial commit

---
+++ whoami/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <h2>Hello World</h2>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
