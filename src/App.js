e409582c726ebf8057e896a76a1eb7e87f895499
Parthiv-M <parthiv.menon@learner.manipal.edu>
2021-07-18 19:39:52 +0530

    router options changed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Page from './pages/Page';
-import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
